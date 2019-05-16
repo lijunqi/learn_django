1b4127118de9a16ee6ebd539a36f521f96a86f7e
lijunqi <lijunqi001@163.com>
2019-05-16 21:46:11 +0800

    first commit. finish part 2.

---
+++ jackyapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JackyappConfig(AppConfig):
+    name = 'jackyapp'
---
+++ jackyapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+'''
+1. Change your models (in models.py).
+2. Run python manage.py makemigrations to create migrations for those changes
+3. Run python manage.py migrate to apply those changes to the database.
+'''
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
