82ae81df8f83ed764753ea31b120806c30cdf856
lijunqi <lijunqi001@163.com>
2019-05-16 22:22:34 +0800

    writing more views

--- jackyapp/urls.py
+++ jackyapp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /jackyapp/
+    path('', views.index, name='index'),
+
+    # ex: /jackyapp/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # ex: /jackyapp/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # ex: /jackyapp/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- jackyapp/views.py
+++ jackyapp/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, Jacky!")
+
+# Add a few more views
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're lokking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
