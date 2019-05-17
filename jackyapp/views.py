8f994b2bf49810abc598140a5adea08b04c3a617
lijunqi <lijunqi001@163.com>
2019-05-17 22:15:07 +0800

    adopt template fearute

--- jackyapp/views.py
+++ jackyapp/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, Jacky!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # The context is a dictionary mapping template variable names to Python objects.
+    context = { 'latest_question_list': latest_question_list, }
+
+    # The render() function takes the request object as its first argument,
+    # a template name as its second argument and a dictionary as its optional
+    # third argument. It returns an HttpResponse object of the given template
+    # rendered with the given context.
+    return render(request, 'polls/index.html', context)
 
 # Add a few more views
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("question does not exist...")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're lokking at the results of question %s."
