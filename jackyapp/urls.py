ce29416789dbedbffe1305897014a527d12b2f75
lijunqi <lijunqi001@163.com>
2019-05-21 20:56:34 +0800

    use generic-views

--- jackyapp/urls.py
+++ jackyapp/urls.py
@@ -5,15 +5,21 @@ from . import views
 #  for a url when using the {% url %} template tag? --> Namespacing URL names
 app_name = 'polls'
 urlpatterns = [
-    # ex: /jackyapp/
-    path('', views.index, name='index'),
+    # Use generic views
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 
-    # ex: /jackyapp/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-
-    # ex: /jackyapp/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    # ex: /jackyapp/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote')
+#    # ex: /jackyapp/
+#    path('', views.index, name='index'),
+#
+#    # ex: /jackyapp/5/
+#    path('<int:question_id>/', views.detail, name='detail'),
+#
+#    # ex: /jackyapp/5/results/
+#    path('<int:question_id>/results/', views.results, name='results'),
+#
+#    # ex: /jackyapp/5/vote/
+#    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
